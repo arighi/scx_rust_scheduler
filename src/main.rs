7239674b0030bdc5dfd263dd93cfa98acbf2c04d
Andrea Righi <andrea.righi@linux.dev>
2024-09-06 09:24:36 +0200

    scx_rust_scheduler: update to scx_rustland_core v2.1
    Signed-off-by: Andrea Righi <andrea.righi@linux.dev>

--- src/main.rs
+++ src/main.rs
@@ -113,7 +113,7 @@ impl<'a> Scheduler<'a> {
             // prioritizing its previously used CPU (task.cpu).
             //
             // If we can't find any idle CPU, keep the task running on the same CPU.
-            let cpu = self.bpf.select_cpu(task.pid, task.cpu, 0);
+            let cpu = self.bpf.select_cpu(task.pid, task.cpu, task.flags);
             dispatched_task.cpu = if cpu < 0 { task.cpu } else { cpu };
 
             // Determine the task's time slice: assign value inversely proportional to the number
